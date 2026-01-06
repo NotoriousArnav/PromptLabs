fe8b6a346c17908829aa444261e9010896cca546
NotoriousArnav <arnavghosh.official.2004@gmail.com>
2026-01-06 18:41:54 +0530

    Added Prompt Model to Database, ORM, Admin Panel and made Migrations

--- prompts/models.py
+++ prompts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Prompt(models.Model):
+    id = models.UUIDField(
+            primary_key=True,
+            default=uuid4,
+            editable=False
+        )
+
+    prompt = models.TextField() # Field that will contain the Prompt
+    tags = models.CharField(max_length=200) # Comma Seperated tags
+    expected_output = models.ImageField(upload_to='media/prompts/expected_output/', blank=True, null=True)
